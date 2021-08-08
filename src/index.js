bc77e369d95a3ae8499020099158ac25e05da2b7
Carlos Urquiza <webquiza@gmail.com>
2021-08-08 12:24:09 -0700

    fix fetch functionality
    Co-authored-by: Shaun Kent <shaunkent81@gmail.com>
    Co-authored-by: Alex Goldberg <alexdominguezwebdev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import { Satellite } from './static';
+
+function getData(response) {
+  console.log(response.id);
+  $('#sat-about').text(`The ${response.name} ID number is ${response.id}`);
+  $('#show-lon').text(`The longitude of the ${response.name} is ${response.longitude}`);
+  $('#show-lat').text(`The latitude of the ${response.name} is ${response.latitude}`);
+}
+
+$(document).ready(function (){
+  $('.button').click(function() {
+    Satellite.getSatellite()
+      .then(function(response) {
+        getData(response);
+      });
+  });
+});
--- src/static.js
+++ src/static.js
@@ -1,5 +1,14 @@
-export default class Satellite {
-    static getSatellite(lon, lat) {
-
-    }
+export class Satellite {
+  static getSatellite() {
+    return fetch(`https://api.wheretheiss.at/v1/satellites/25544`)
+      .then(function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
 }
